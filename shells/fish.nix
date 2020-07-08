ce71274eddf7317e6a8f7c0a2d3b74d611442c1b
hlolli <hlolli@gmail.com>
2020-07-08 21:11:31 +0200

    add decleratively fish function `ll`

--- shells/fish.nix
+++ shells/fish.nix
@@ -1,7 +1,16 @@
 { config, lib, pkgs, ... }:
-{
+
+let interactiveShellInit = ''
+  function ll
+    ${pkgs.exa}/bin/exa --all --long --header --grid $argv
+  end
+'';
+
+in {
   config = {
     programs.fish = {
+      inherit interactiveShellInit;
+      vendor.functions.enable = false;
       enable = true;
       promptInit = ''
         ${pkgs.any-nix-shell}/bin/any-nix-shell fish --info-right | source
